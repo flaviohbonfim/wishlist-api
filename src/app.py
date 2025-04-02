30416bd38b631534510d146ba18a3b76c1daa8c5
Flávio Henrique Bonfim <flaviohbonfim@live.com>
2025-04-02 18:23:42 -0300

    small adjust on fastapi title info

--- src/app.py
+++ src/app.py
@@ -8,7 +8,13 @@ from .routers import auth, user, wishlist
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-app = FastAPI()
+app = FastAPI(
+    title='Wishlist API',
+    description='API para controle de produtos favoritos dos clientes.',
+    version='1.0.0',
+    docs_url='/docs',
+    redoc_url='/redoc',
+)
 
 app.include_router(user.router)
 app.include_router(auth.router)
